b06d8505ffb0dd2dbc111117655c72d2ebc53c5e
wanzhiwen <977242261@qq.com>
2019-02-23 11:25:50 +0800

    Longest Common Prefix

--- src/LeetCode/Algorithm/q14.java
+++ src/LeetCode/Algorithm/q14.java
@@ -8,12 +8,12 @@ public class q14 {
 
     //best java version
     public static String longestCommonPrefix1(String[] strs) {
-        if(strs == null || strs.length == 0)    return "";
+        if (strs == null || strs.length == 0) return "";
         String pre = strs[0];
         int i = 1;
-        while(i < strs.length){
-            while(strs[i].indexOf(pre) != 0)
-                pre = pre.substring(0,pre.length()-1);
+        while (i < strs.length) {
+            while (strs[i].indexOf(pre) != 0)
+                pre = pre.substring(0, pre.length() - 1);
             i++;
         }
         return pre;
@@ -22,7 +22,7 @@ public class q14 {
     //my version
     public static String longestCommonPrefix(String[] strs) {
         StringBuilder res = new StringBuilder();
-        if (strs.length==0||(strs.length == 1 && strs[0].length()==0)) {
+        if (strs.length == 0 || (strs.length == 1 && strs[0].length() == 0)) {
             return "";
         }
         int minLen = Integer.MAX_VALUE;
@@ -33,7 +33,7 @@ public class q14 {
         }
         for (int i = 0; i < minLen; i++) {
             for (int j = 0; j < strs.length; j++) {
-                if (strs[j].charAt(i)!=strs[0].charAt(i)){
+                if (strs[j].charAt(i) != strs[0].charAt(i)) {
                     return res.toString();
                 }
             }
@@ -43,7 +43,7 @@ public class q14 {
     }
 
     public static void main(String[] args) {
-        String[] strings={"a"};
+        String[] strings = {"a"};
         System.out.println(longestCommonPrefix(strings));
         //System.out.println(strings.length);
     }

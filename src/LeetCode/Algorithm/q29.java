c906ed0da86876b525d011214f7e8e6c5dbe2452
wanzhiwen <977242261@qq.com>
2019-03-04 14:55:01 +0800

    Divide Two Integers

--- src/LeetCode/Algorithm/q29.java
+++ src/LeetCode/Algorithm/q29.java
@@ -6,9 +6,29 @@ package LeetCode.Algorithm;
  */
 public class q29 {
 
+    public static int divide(int dividend, int divisor) {
+        int sign = 1;
+        if (dividend > 0 && divisor < 0 || dividend < 0 && divisor > 0) sign = -1;
+        long ldividend = Math.abs((long) dividend);
+        long ldivisor = Math.abs((long) divisor);
+        long ans = ldivide(ldividend, ldivisor);
+        if (sign == 1 && ans > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if (sign==1)return (int) ans;
+        return (int) -ans;
+    }
 
+    private static long ldivide(long ldividend, long ldivisor) {
+        if (ldividend < ldivisor) return 0;
+        long sum = ldivisor;
+        int multiple = 1;
+        while ((sum + sum) < ldividend) {
+            multiple += multiple;
+            sum += sum;
+        }
+        return multiple + ldivide(ldividend - sum, ldivisor);
+    }
 
     public static void main(String[] args) {
-
+        System.out.println(divide(-4,1));
     }
 }

a1121fabdb4bd7e5a9978164a55d8abf76108a65
wanzhiwen <977242261@qq.com>
2019-04-09 09:53:25 +0800

    最大序列和

---
+++ src/NIUKE/KAOYAN/MaxSeq.java
@@ -0,0 +1,26 @@
+package NIUKE.KAOYAN;
+
+import java.util.*;
+
+/**
+ * @author wanzhiwen
+ * @time 2019/4/9
+ */
+public class MaxSeq {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num, seq, max, sum;
+        while (sc.hasNext()) {
+            max = Integer.MIN_VALUE;
+            sum = 0;
+            num = sc.nextInt();
+            for (int i = 0; i < num; i++) {
+                seq = sc.nextInt();
+                sum += seq;
+                if (sum > max) max = sum;
+                if (sum < 0) sum = 0;
+            }
+            System.out.println(max);
+        }
+    }
+}

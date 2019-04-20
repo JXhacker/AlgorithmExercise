03b4c07fad856373130da893c450d33f6c32f8a9
wanzhiwen <977242261@qq.com>
2019-04-20 11:44:39 +0800

    进制转化加强版

---
+++ src/NIUKE/KAOYAN/HexadecimalConversion_plus.java
@@ -0,0 +1,19 @@
+package NIUKE.KAOYAN;
+
+import java.util.*;
+
+/**
+ * @author wanzhiwen
+ * @time 2019/4/20
+ */
+public class HexadecimalConversion_plus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int radix1 = sc.nextInt();
+            String num = sc.next();
+            int radix2 = sc.nextInt();
+            System.out.println(Long.toString(Long.parseLong(num, radix1), radix2).toUpperCase());
+        }
+    }
+}

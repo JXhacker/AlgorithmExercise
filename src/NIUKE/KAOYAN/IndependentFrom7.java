016d860aa83316b1c3cabc469c5d218f0fe4dfca
wanzhiwen <977242261@qq.com>
2019-04-20 11:16:34 +0800

    与7无关的数

---
+++ src/NIUKE/KAOYAN/IndependentFrom7.java
@@ -0,0 +1,24 @@
+package NIUKE.KAOYAN;
+
+import java.util.*;
+
+/**
+ * @author wanzhiwen
+ * @time 2019/4/20
+ */
+public class IndependentFrom7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N, ans;
+        while (sc.hasNext()) {
+            N = sc.nextInt();
+            ans = 0;
+            for (int i = 1; i <= N; i++) {
+                if (i % 7 != 0 && String.valueOf(i).indexOf("7") == -1) {
+                    ans += Math.pow(i, 2);
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+}

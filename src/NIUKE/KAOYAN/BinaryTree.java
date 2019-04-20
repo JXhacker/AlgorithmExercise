f0d5f171a62be19018080a52ebef6faa8704a9f5
wanzhiwen <977242261@qq.com>
2019-04-20 10:36:36 +0800

    二叉树

---
+++ src/NIUKE/KAOYAN/BinaryTree.java
@@ -0,0 +1,23 @@
+package NIUKE.KAOYAN;
+
+import java.util.*;
+
+/**
+ * @author wanzhiwen
+ * @time 2019/4/20
+ */
+public class BinaryTree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x, y;
+        while (sc.hasNext()) {
+            x = sc.nextInt();
+            y = sc.nextInt();
+            while (x != y) {
+                if (x > y) x /= 2;
+                else y /= 2;
+            }
+            System.out.println(x);
+        }
+    }
+}

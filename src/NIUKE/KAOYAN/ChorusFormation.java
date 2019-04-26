0ed885a3d8c6b7bfb35cca5dc23c25341bb5edc9
wanzhiwen <977242261@qq.com>
2019-04-26 23:15:44 +0800

    合唱队形 动态规划

--- src/NIUKE/KAOYAN/ChorusFormation.java
+++ src/NIUKE/KAOYAN/ChorusFormation.java
@@ -31,14 +31,36 @@ public class ChorusFormation {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()) {
-            int N = sc.nextInt();
-            List<Integer> height = new ArrayList<>();
-            for (int i = 0; i < N; i++) {
-                height.add(sc.nextInt());
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            int n = in.nextInt();
+            int[] arr = new int[n];
+            int[] left = new int[n];
+            int[] right = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = in.nextInt();
             }
-            System.out.println(Maxqueue(0, N - 1, height));
+            //动态规划
+            //求最长递增子序列
+            for (int i = 0; i < n; i++) {
+                left[i] = 1;
+                for (int j = i - 1; j >= 0; j--)
+                    if (arr[j] < arr[i])
+                        left[i] = Math.max(left[i], left[j] + 1);
+            }
+
+            //求从右边数起的最长递减子序列
+            for (int i = n - 1; i >= 0; i--) {
+                right[i] = 1;
+                for (int j = i + 1; j < n; j++)
+                    if (arr[i] > arr[j])
+                        right[i] = Math.max(right[i], right[j] + 1);
+            }
+            int max = 0;
+            for (int i = 0; i < n; i++) {
+                max = Math.max(max, left[i] + right[i] - 1);
+            }
+            System.out.println(n - max);
         }
     }
 }

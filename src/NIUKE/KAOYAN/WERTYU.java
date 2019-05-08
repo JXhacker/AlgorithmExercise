b98c81c44a038b6b066d3cf45c702c5bad27217e
wanzhiwen <977242261@qq.com>
2019-05-08 09:02:54 +0800

    WERTYU

---
+++ src/NIUKE/KAOYAN/WERTYU.java
@@ -0,0 +1,27 @@
+package NIUKE.KAOYAN;
+
+import java.util.*;
+
+/**
+ * @author wanzhiwen
+ * @Date 2019/5/8
+ */
+public class WERTYU {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String keyboard = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            StringBuilder ans = new StringBuilder();
+            for (int i = 0; i < input.length(); i++) {
+                if (input.charAt(i) == ' ') {
+                    ans.append(" ");
+                } else {
+                    int index = keyboard.indexOf(input.charAt(i));
+                    ans.append(String.valueOf(keyboard.charAt(index - 1)));
+                }
+            }
+            System.out.println(ans.toString());
+        }
+    }
+}
